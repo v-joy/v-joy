d2b6703218d4393381a2a9ddd454aff0f75f854d
xiao.chen <companyforme@gmail.com>
2015-05-10 14:37:26 +0800

    index add

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,71 +1,90 @@
-<?php
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
-
-/* @var $this \yii\web\View */
-/* @var $content string */
-
-AppAsset::register($this);
-?>
-<?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html>
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <meta charset="utf-8"/>
+    <title>水果商店</title>
+    <link rel="stylesheet" type="text/css" href="/web/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet/less" type="text/css" href="/web/less/all-style.less" />
+
+    <!-- set options before less.js script -->
+    <script>
+        less = {
+            env: "development",
+            async: false,
+            fileAsync: false,
+            poll: 1000,
+            functions: {},
+            dumpLineNumbers: "comments",
+            relativeUrls: false,
+            rootpath: "/"
+        };
+    </script>
+    <script src="/web/js/common/less.min.js" type="text/javascript"></script>
 </head>
 <body>
-
-<?php $this->beginBody() ?>
-    <div class="wrap">
-        <?php
-            NavBar::begin([
-                'brandLabel' => 'My Company',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'signup', 'url' => ['/site/signup']],
-                    ['label' => 'forget', 'url' => ['/site/RequestPasswordReset']],
-                    //['label' => 'Contact', 'url' => ['/site/contact']],
-                    Yii::$app->user->isGuest ?
-                        ['label' => 'Login', 'url' => ['/site/login']] :
-                        ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                            'url' => ['/site/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
-                ],
-            ]);
-            NavBar::end();
-        ?>
-
-        <div class="container">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= $content ?>
+<div id="header">
+    <div class="logo-wrap">
+        <a class="logo" href="/web/">logo</a>
+        <div class="f-right search-wrap">
+            <input type="text" placeholder="请输入所查询商品名"/>
+            <input type="submit" value="搜 索" />
         </div>
     </div>
-
-    <footer class="footer">
-        <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-            <p class="pull-right"><?= Yii::powered() ?></p>
+    <div class="nav-wrap">
+        <div class="nav">
+            <ul>
+                <li><a href="#">首页</a></li>
+                <li><a href="#">商品</a></li>
+            </ul>
+            <div class="login-wrap">
+                <a href="#">登录</a>
+                <a href="#">注册</a>
+            </div>
         </div>
-    </footer>
+    </div>
+</div>
+<div id="main">
 
-<?php $this->endBody() ?>
+</div>
+<div id="footer">
+    <hr/>
+    <div class="info-box clearfix">
+        <div class="info-item f-left">
+            <h3>商务合作</h3>
+            <ul>
+                <li><a href="#">提供商家信息</a></li>
+                <li><a href="#">提供商家信息</a></li>
+                <li><a href="#">提供商家信息</a></li>
+            </ul>
+        </div>
+        <div class="info-item f-left">
+            <h3>商务合作</h3>
+            <ul>
+                <li><a href="#">提供商家信息</a></li>
+                <li><a href="#">提供商家信息</a></li>
+                <li><a href="#">提供商家信息</a></li>
+            </ul>
+        </div>
+        <div class="info-item f-left">
+            <h3>商务合作</h3>
+            <ul>
+                <li><a href="#">提供商家信息</a></li>
+                <li><a href="#">提供商家信息</a></li>
+                <li><a href="#">提供商家信息</a></li>
+            </ul>
+        </div>
+        <div class="info-item f-left">
+            <h3>商务合作</h3>
+            <ul>
+                <li><a href="#">提供商家信息</a></li>
+                <li><a href="#">提供商家信息</a></li>
+                <li><a href="#">提供商家信息</a></li>
+            </ul>
+        </div>
+    </div>
+    <p>Copyright:©2015美团网团购 meituan.com 京ICP证070791号 京公网安备11010502025545号 电子公告服务规则s</p>
+</div>
+<script src="/web/js/layouts/main.js"></script>
+<script src="/web/js/common/angular.min.js"></script>
 </body>
 </html>
-<?php $this->endPage() ?>
